102b10e3592661373725c286650bdb58ccad35d7
Joel Fregno <joelfregno@outlook.com>
2024-02-07 16:24:16 +0100

    watch list app

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Lists
+  get "/lists", to: "lists#index"
+  get "/lists/new", to: "lists#new", as: :new_list
+  post "/lists", to: "lists#create"
+  get "/lists/:id", to: "lists#show", as: :list
+  get "/lists/:list_id/bookmarks/new", to: "bookmarks#new", as: :new_list_bookmark
+  post "/lists/:list_id/bookmarks", to: "bookmarks#create", as: :list_bookmarks
+
+  # Bookmarks
+  delete "/bookmarks/:id", to: "bookmarks#destroy", as: :bookmark
+
+  # Reviews
+  delete "/reviews/:id", to: "reviews#destroy", as: :review
 end
